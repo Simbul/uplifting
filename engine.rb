eeb6b6c063395df1e468e055eaab87bc86801b48
Alessandro Morandi <alessandro.morandi@simplybusiness.co.uk>
2013-02-24 22:39:57 +0000

    Refactored game engine into a separate class

---
+++ engine.rb
@@ -0,0 +1,102 @@
+require 'i18n'
+require 'active_support/all'
+
+require_relative 'person'
+require_relative 'elevator'
+
+class Engine
+
+  attr_reader :people
+
+  def initialize(elevator_source, script_source, options)
+    @elevator_source = elevator_source
+    @script_source = script_source
+    @options = options
+
+    @elevator_class = elevator_class(elevator_source)
+    @script_module = script_module(script_source)
+
+    @people = []
+    @prng = options.seed ? Random.new(options.seed) : Random.new
+    @duration = @script_module::SIMULATION[:ticks]
+    @elevators = @script_module::ELEVATORS.map { |name, params| @elevator_class.new(name, params[:ticks_per_floor], params[:capacity], params[:floors]) }
+    @script = @script_module::SCRIPT
+  end
+
+  def run
+    @duration.times do |time|
+
+      puts " #{time.to_s.rjust(@duration.to_s.length)} ".center(70, '=')
+
+      # Running script
+      if declarative?
+        if events = @script[:events][time]
+          events.each do |event|
+            case event.first
+            when :spawn_person
+              from_floor = event[1]
+              to_floor = event[2]
+              puts "! spawned person: #{from_floor} -> #{to_floor}"
+              @people << Person.new(from_floor, to_floor, time)
+            end
+          end
+        end
+      elsif random?
+        if @prng.rand < Script::SCRIPT[:spawn_ratio]
+          from_floor = (@prng.rand * top_floor).round
+          to_floor = (@prng.rand * top_floor).round
+          puts "! spawned person: #{from_floor} -> #{to_floor}"
+          @people << Person.new(from_floor, to_floor, time)
+        end
+      end
+
+      # People actions
+      @people.each{ |person| person.act(time, @elevators) }
+
+      # Elevator commands
+      @elevators.each do |elevator|
+        elevator.process(time)
+      end
+
+    end
+  end
+
+  def declarative?
+    @script[:type] == :declarative
+  end
+
+  def random?
+    @script[:type] == :random
+  end
+
+  def seed
+    @prng.seed
+  end
+
+  def arrived; @people.select{ |person| person.arrived? }; end
+  def travelling; @people.select{ |person| person.on_elevator? }; end
+  def waiting; @people.select{ |person| person.waiting? }; end
+
+
+  private
+
+  def elevator_class(elevator_source)
+    require_relative elevator_source
+    klass = elevator_source.sub('.rb', '').classify.constantize
+    unless klass.superclass == Elevator
+      raise ArgumentError, "Class #{klass} in #{elevator_source} should be a subclass of Elevator"
+    end
+    klass
+  end
+
+  def script_module(script_source)
+    require_relative script_source
+    raise ArgumentError, "#{script_source} should contain a Script module" unless defined?(Script)
+    Script
+  end
+
+  def top_floor
+    @elevators.max{ |elevator| elevator.floors }.floors
+  end
+
+end
--- master.rb
+++ master.rb
@@ -2,11 +2,8 @@
 
 require 'optparse'
 require 'ostruct'
-require 'i18n'
-require 'active_support/all'
 
-require_relative 'person'
-require_relative 'elevator'
+require_relative 'engine'
 
 options = OpenStruct.new
 optparse = OptionParser.new do |opts|
@@ -23,78 +20,23 @@ if ARGV.size < 2
   exit(-1)
 end
 
-elevator_source = ARGV[0]
-require_relative elevator_source
-elevator_class = elevator_source.sub('.rb', '').classify.constantize
-
-unless elevator_class.superclass == Elevator
-  puts "Class #{elevator_class} in #{elevator_source} should be a subclass of Elevator"
-  puts optparse
-  exit(-1)
-end
-
-script_source = ARGV[1]
-require_relative script_source
-
-people = []
-prng = options.seed ? Random.new(options.seed) : Random.new
-duration = Script::SIMULATION[:ticks]
-elevators = Script::ELEVATORS.map { |name, params| elevator_class.new(name, params[:ticks_per_floor], params[:capacity], params[:floors]) }
-script = Script::SCRIPT
-
-def top_floor(elevators)
-  elevators.max{ |elevator| elevator.floors }.floors
-end
-
-duration.times do |time|
-
-  puts " #{time.to_s.rjust(duration.to_s.length)} ".center(70, '=')
-
-  # Running script
-  if script[:type] == :declarative
-    if events = script[:events][time]
-      events.each do |event|
-        case event.first
-        when :spawn_person
-          from_floor = event[1]
-          to_floor = event[2]
-          puts "! spawned person: #{from_floor} -> #{to_floor}"
-          people << Person.new(from_floor, to_floor, time)
-        end
-      end
-    end
-  elsif script[:type] == :random
-    if prng.rand < Script::SCRIPT[:spawn_ratio]
-      from_floor = (prng.rand * top_floor(elevators)).round
-      to_floor = (prng.rand * top_floor(elevators)).round
-      puts "! spawned person: #{from_floor} -> #{to_floor}"
-      people << Person.new(from_floor, to_floor, time)
-    end
-  end
-
-  # People actions
-  people.each{ |person| person.act(time, elevators) }
-
-  # Elevator commands
-  elevators.each do |elevator|
-    elevator.process(time)
-  end
-
-end
-
-# Stats
-arrived = people.select{ |person| person.arrived? }
-travelling = people.select{ |person| person.on_elevator? }
-waiting = people.select{ |person| person.waiting? }
-
 def singularize(num, word)
   (num == 1) ? "#{num} #{word.singularize}" : "#{num} #{word}"
 end
 
-puts
-puts " Simulation finished ".center(70, '*')
-puts "Randomized with seed #{prng.seed}" if script[:type] == :random
-puts "#{singularize(arrived.count, 'people')} arrived at destination"
-puts "#{singularize(travelling.count, 'people')} still travelling"
-puts "#{singularize(waiting.count, 'people')} still waiting for an elevator"
-puts "Score: #{arrived.count * 2 - travelling.count - waiting.count * 2}"
+begin
+  engine = Engine.new(ARGV[0], ARGV[1], options)
+  engine.run
+
+  puts
+  puts " Simulation finished ".center(70, '*')
+  puts "Randomized with seed #{engine.seed}" if engine.random?
+  puts "#{singularize(engine.arrived.count, 'people')} arrived at destination"
+  puts "#{singularize(engine.travelling.count, 'people')} still travelling"
+  puts "#{singularize(engine.waiting.count, 'people')} still waiting for an elevator"
+  puts "Score: #{engine.arrived.count * 2 - engine.travelling.count - engine.waiting.count * 2}"
+rescue ArgumentError => e
+  puts "ERROR: #{e.message}"
+  puts optparse
+  exit(-1)
+end
